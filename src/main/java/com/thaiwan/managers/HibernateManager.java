1c78dd42a073437b0c3d740f63a7061a2ddf2b00
Tatiana Mitioglova <thaiwan7@gmail.com>
2017-01-21 01:41:56 +0300

    added HibernateManager

---
+++ src/main/java/com/thaiwan/managers/HibernateManager.java
@@ -0,0 +1,44 @@
+package com.thaiwan.managers;
+
+import com.thaiwan.entities.Bus;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ * Created by Tanya on 01.12.2016.
+ */
+public class HibernateManager {
+    private SessionFactory sessionFactory = buildSessionFactory();
+    private static HibernateManager instance;
+
+    private HibernateManager() {
+    }
+
+    public static HibernateManager getInstance() {
+        if (instance == null) {
+            instance = new HibernateManager();
+        }
+        return instance;
+    }
+
+    private SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).buildServiceRegistry();
+
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    public void close() {
+        sessionFactory.close();
+    }
+}
